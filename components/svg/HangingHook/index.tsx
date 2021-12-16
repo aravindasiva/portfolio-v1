c97651a309d57eb26dd915d32bcf301e95d2e08c
aravindasiva <aravinda.siva@pego.co.uk>
2021-12-16 11:09:32 +0000

    404 page away for christmas

---
+++ components/svg/HangingHook/index.tsx
@@ -0,0 +1,33 @@
+import * as React from "react";
+
+const HangingHook = (props) => (
+  <svg
+    width={15}
+    height={61}
+    viewBox="0 0 15 61"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <path
+      d="M7.36438 0.744141V38.7441"
+      stroke="#D6DCE8"
+      strokeWidth={2}
+      strokeMiterlimit={10}
+      strokeLinejoin="round"
+    />
+    <path
+      d="M7.44349 44.4436C10.5468 44.4436 13.0625 41.9279 13.0625 38.8246C13.0625 35.7213 10.5468 33.2056 7.44349 33.2056C4.34019 33.2056 1.82446 35.7213 1.82446 38.8246C1.82446 41.9279 4.34019 44.4436 7.44349 44.4436Z"
+      fill="#D6DCE8"
+    />
+    <path
+      d="M1.98279 53.7824C1.98279 57.2647 5.14845 60.1138 8.94725 59.1641C11.0049 58.6892 12.5878 57.1064 13.0626 55.0487C13.6957 52.0413 11.9546 49.1922 9.26381 48.4008C8.31411 48.0842 7.68098 47.2928 7.68098 46.1848V41.2781"
+      stroke="#D6DCE8"
+      strokeWidth={2}
+      strokeMiterlimit={10}
+      strokeLinejoin="round"
+    />
+  </svg>
+);
+
+export default HangingHook;
