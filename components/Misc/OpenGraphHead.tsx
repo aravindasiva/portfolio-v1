86a2fa009807d58467fe55826b51572b24274d1c
aravindasiva <aravindcva@hotmail.com>
2022-01-04 21:28:32 +0000

    added head

---
+++ components/Misc/OpenGraphHead.tsx
@@ -0,0 +1,20 @@
+import Head from 'next/head'
+const OpenGraphHead = () => (
+  <Head>
+    <title>Aravinda Siva | Full Stack Developer</title>
+    <meta name="description" content="My tiny personal space on the world of web." />
+    <meta property="og:title" content="Aravinda Siva | Full Stack Developer" />
+    <meta property="og:site_name" content="Aravinda Siva" />
+    <meta property="og:url" content="https://aravindasiva.com/" />
+    <meta
+      property="og:description"
+      content="Hey! Nice of you to visit my personal site, Thank you! I am a Full Stack developer"
+    />
+    <meta property="og:type" content="profile" />
+    <meta
+      property="og:image"
+      content="https://aravindasiva.com/preview.png"
+    ></meta>
+  </Head>
+)
+export default OpenGraphHead
